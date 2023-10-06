028177a7ef61abce15728e855519af64702113d0
wang <wanglook01@outlook.com>
2023-10-06 23:35:22 +0800

    项目初始化

---
+++ demo-grpc/src/main/java/com/wanglook01/DemoGrpcApplication.java
@@ -0,0 +1,12 @@
+package com.wanglook01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGrpcApplication.class, args);
+    }
+}
