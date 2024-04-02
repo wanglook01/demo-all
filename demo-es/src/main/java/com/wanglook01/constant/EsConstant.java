d0818ff5fd8a4605f84c13184cade3e02a28c374
wang002 <wanglook01@outlook.com>
2024-04-02 15:35:22 +0800

    es demo

--- demo-es/src/main/java/com/wanglook01/constant/EsConstant.java
+++ demo-es/src/main/java/com/wanglook01/constant/EsConstant.java
@@ -3,5 +3,5 @@ package com.wanglook01.constant;
 public class EsConstant {
 
 
-    public static final String INDEX_SKU = "search_sku_index";
+    public static final String INDEX_SKU = "sku_index";
 }
